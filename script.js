9c2dd27f8e7b138271ad8539717f0b3a17cf0ef1
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-23 19:59:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+window.onload=function(){getTime();}  
+function getTime(){  
+var today=new Date(); 
+var date=today.getDate();
+var month=today.getMonth();
+var year=today.getFullYear();
+var hours=today.getHours();
+var minutes=today.getMinutes();
+var seconds=today.getSeconds();
+var version="AM";
+	if(hours>=12)
+	{
+		version="PM"
+	}
+document.getElementById('timer').innerHTML=(month+"/"+date+"/"+year+", "+hours+":"+minutes+":"+seconds+" "+version) ;
+setTimeout(function(){getTime()},1000);  
+}  
+//setInterval("getTime()",1000);//another way  
+function checkTime(i){  
+if (i<10){  
+  i="0" + i;  
+ }  
+return i;  
+}  
